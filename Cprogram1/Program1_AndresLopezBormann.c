1a3c79b464dccf9ab1caadf7a245f9a4e25ff16c
Andres Lopez-Bormann <75829728+AndresLopezBormann@users.noreply.github.com>
2023-02-12 09:58:27 -0600

    Commit of CProgram1_AndresLoezBormann

---
+++ Cprogram1/Program1_AndresLopezBormann.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Program1_functions.h"
+
+int main(void){
+    printMothlySales();
+    printSalesSummary();
+    SixMonthAverageReport();
+    reportHighToLow();
+}
